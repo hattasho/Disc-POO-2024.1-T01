7277e2deb7f837cd91a5b95b34fe4bab05cd6b3f
hattsaull <saul.ramos07@aluno.ifce.edu.br>
2024-03-26 19:54:09 -0300

    Add files via upload

--- CAP-03/Exercícios-Propostos/QP05/src/br/edu/principal/Principal.java
+++ CAP-03/Exercícios-Propostos/QP05/src/br/edu/principal/Principal.java
@@ -7,10 +7,10 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n1 = 0;
-		System.out.print("Escolha um número de 0 a 10. Digite aqui: ");
+		System.out.print("Escolha um nÃºmero de 0 a 10. Digite aqui: ");
 		n1 = sc.nextInt();
 		if(n1<0 || n1>10) {
-			System.out.print("O número não está entre 0 e 10. Tente novamente.");
+			System.out.print("O nÃºmero nÃ£o estÃ¡ entre 0 e 10. Tente novamente.");
 		} else {
 			for (int i = 0; i <= 10; i++){
 				System.out.println(n1+" x "+i+" = "+(n1*i));
